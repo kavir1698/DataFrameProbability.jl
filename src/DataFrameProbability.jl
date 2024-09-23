7d521c83ed263da0bf5f52538088df09ba2ee0a2
Farhad Vahdati <arv.kavir@gmail.com>
2024-09-23 10:25:38 +0200

    feat: Update calculate_probability_distribution function to support explicit bin boundaries for n_bins parameter

--- src/DataFrameProbability.jl
+++ src/DataFrameProbability.jl
@@ -9,7 +9,7 @@ using CategoricalArrays
 export calculate_probability_distribution, compare_distributions
 
 """
-    calculate_probability_distribution(df::DataFrame, focal_column::Symbol, condition_columns::Vector{Symbol} = Symbol[]; n_bins::Union{Int, Vector{Int}} = 10, column_types::Dict{Symbol, Symbol} = Dict{Symbol, Symbol}(), column_ranges::Dict{Symbol, Tuple{Real, Real}} = Dict{Symbol, Tuple{Real, Real}}(), all_categories::Dict{Symbol, Vector} = Dict{Symbol, Vector}()) -> Tuple{Union{Weights, Dict}, Dict{Symbol, Vector}}
+    calculate_probability_distribution(df::DataFrame, focal_column::Symbol, condition_columns::Vector{Symbol} = Symbol[]; n_bins::Union{Int, Dict{Symbol,Int}, Dict{Symbol,Vector{<:Real}}} = 5, column_types::Dict{Symbol, Symbol} = Dict{Symbol, Symbol}(), column_ranges::Dict{Symbol, Tuple{Real, Real}} = Dict{Symbol, Tuple{Real, Real}}(), all_categories::Dict{Symbol, Vector} = Dict{Symbol, Vector}(), count_focal_occurrences::Bool = false) -> Tuple{Union{Weights, Dict}, Dict{Symbol, Vector}}
 
 Calculate the probability distribution of a focal column in a DataFrame, optionally conditioned on other columns.
 
@@ -17,7 +17,7 @@ Calculate the probability distribution of a focal column in a DataFrame, optiona
 - `df::DataFrame`: The DataFrame to calculate the probability distribution from.
 - `focal_column::Symbol`: The column to calculate the probability distribution for.
 - `condition_columns::Vector{Symbol}`: The columns to condition the probability distribution on. If empty, the marginal distribution of the focal column is calculated.
-- `n_bins::UUnion{Int,Dict{Symbol,Int}}=5`: The number of bins to use for discretization. If an integer is provided, it is used for all columns. If a dictionary is provided, each element is used for the corresponding column.
+- `n_bins::Union{Int, Dict{Symbol,Int}, Dict{Symbol,Vector{<:Real}}}=5`: The number of bins or explicit bin boundaries to use for discretization. If an integer is provided, it is used for all columns. If a dictionary is provided, each element can be either an integer (number of bins) or a vector of bin boundaries for the corresponding column. The format of vector of bins is the same as the `edges` argument in `Discretizers.LinearDiscretizer`. Briefly, it should include the edges of the bins in ascending order. The first and last elements are the lower and upper bounds of the first and last bins, respectively. The number of bins is equal to the length of the vector minus one.
 - `column_types::Dict{Symbol, Symbol}`: A dictionary mapping column names to their types. The types can be `:continuous` or `:categorical`. If a column is not found in the dictionary, it is considered `:continuous` by default.
 - `column_ranges::Dict{Symbol, Tuple{Real, Real}}`: A dictionary mapping column names to their min and max possible value. The ranges are used for discretization. If a column is not found in the dictionary, the minimum and maximum values of the column are used.
 - `all_categories::Dict{Symbol, Vector}`: A dictionary mapping column names to all possible categories for the focal column. If not provided, it will be calculated from the data.
@@ -28,10 +28,11 @@ Calculate the probability distribution of a focal column in a DataFrame, optiona
 """
 function calculate_probability_distribution(
   df::DataFrame, focal_column::Symbol, condition_columns::Vector{Symbol}=Symbol[];
-  n_bins::Union{Int,Dict{Symbol,Int}}= 5,
+  n_bins::Union{Int,Dict{Symbol,Int},Dict{Symbol,Vector{T}}}=5,
   column_types::Dict{Symbol,Symbol}=Dict{Symbol,Symbol}(),
   column_ranges::Dict{Symbol,Tuple{Real,Real}}=Dict{Symbol,Tuple{Real,Real}}(),
-  all_categories::Dict{Symbol,Vector}=Dict{Symbol,Vector}(), count_focal_occurrences::Bool=false)
+  all_categories::Dict{Symbol,Vector}=Dict{Symbol,Vector}(),
+  count_focal_occurrences::Bool=false) where {T<:Real}
 
   # 1. Preprocess the data
   processed_df, updated_all_categories = preprocess_data(
@@ -51,26 +52,33 @@ function calculate_probability_distribution(
 end
 
 function preprocess_data(
-  df::DataFrame, columns::Vector{Symbol}; n_bins::Union{Int,Dict{Symbol,Int}},
+  df::DataFrame, columns::Vector{Symbol};
+  n_bins::Union{Int,Dict{Symbol,Int},Dict{Symbol,Vector{T}}},
   column_types::Dict{Symbol,Symbol}=Dict{Symbol,Symbol}(),
   column_ranges::Dict{Symbol,Tuple{Real,Real}}=Dict{Symbol,Tuple{Real,Real}}(),
-  all_categories::Dict{Symbol,Vector}=Dict{Symbol,Vector}())
+  all_categories::Dict{Symbol,Vector}=Dict{Symbol,Vector}()) where {T<:Real}
+
   processed_df = select(df, columns)
   updated_all_categories = copy(all_categories)
 
-  @assert length(columns) == length(n_bins) || length(n_bins) == 1 "n_bins must be a scalar or a vector of the same length as columns"
-
-  for (i, col) in enumerate(columns)
-    bin = isa(n_bins, Int) ? n_bins : get(n_bins, col, 5)  # default to 5 bins if not specified in the dictionary
-
+  for col in columns
     if get(column_types, col, :continuous) == :categorical || eltype(df[!, col]) <: Bool
       processed_df[!, col] = categorical(processed_df[!, col])
       if !haskey(updated_all_categories, col)
         updated_all_categories[col] = unique(processed_df[!, col])
       end
     elseif eltype(df[!, col]) <: Number
-      min_val, max_val = get(column_ranges, col, (minimum(df[!, col]), maximum(df[!, col])))
-      discretizer = LinearDiscretizer(range(min_val, max_val; length=bin + 1))
+      bin = isa(n_bins, Int) ? n_bins : get(n_bins, col, 5)
+
+      if isa(bin, Int)
+        min_val, max_val = get(column_ranges, col, (minimum(df[!, col]), maximum(df[!, col])))
+        discretizer = LinearDiscretizer(range(min_val, max_val; length=bin + 1))
+      elseif isa(bin, Vector)
+        discretizer = LinearDiscretizer(bin)
+      else
+        error("Invalid bin specification for column $col")
+      end
+
       processed_df[!, col] = categorical(encode(discretizer, df[!, col]))
       if !haskey(updated_all_categories, col)
         updated_all_categories[col] = unique(processed_df[!, col])
